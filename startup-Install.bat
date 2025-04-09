ee64df4e3e9182e7f08fa2b568601f8141377269
SugarcaneDefender <zwaif77@gmail.com>
2025-04-09 17:29:12 -0400

    Fortified install
    Fortified install, to re-run with pip upgrade commands on failure

--- startup-Install.bat
+++ startup-Install.bat
@@ -14,11 +14,6 @@ REM Create and activate the main virtual environment
 python -m venv venv
 call venv\Scripts\activate
 
-
-REM Uncomment / Delete the "REM" in the line below - it may fix some issues you have with your install
-REM python -m pip install --upgrade pip
-
-
 REM Install PyTorch, torchvision, and torchaudio from a specific index URL
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 2>> "%LOG_FILE%"
 
@@ -39,10 +34,31 @@ python main.py 2>> "%LOG_FILE%"
 
 echo:
 echo Z-Waif has stopped running! Likely from an error causing a crash...
-echo See the log.txt file for more info!
+echo Re-running install - Stage II! Running with enforced pip upgrades to ensure proper install! Press enter to continue!
 pause
 
+REM Pip fixes, hope this works
+python -m pip install --upgrade setuptools
+python -m ensurepip --upgrade
+python -m pip install --upgrade pip
 
+REM Install PyTorch, torchvision, and torchaudio from a specific index URL
+python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 2>> "%LOG_FILE%"
+
+REM Install openai-whisper from the GitHub repository
+python -m pip install git+https://github.com/openai/whisper.git 2>> "%LOG_FILE%"
+
+REM Needed upgrades that won't install normally
+python -m pip install --upgrade pywin32
+
+REM Install the remaining dependencies from requirements.txt
+python -m pip install -r requirements.txt 2>> "%LOG_FILE%"
+
+echo:
+echo Z-Waif has stopped running! Likely from an error causing a crash...
+echo Installation failed! Ensure only Python 3.11 is installed, and ensure Git is installed on the system as well!
+echo See log.txt for detailed crash information!
+pause
 
 REM Deactivate the virtual environment
 deactivate
