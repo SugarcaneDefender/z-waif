b0e0351c09f5fafe6ec28b0226fa5b9e92df656c
Henry <cootshk@duck.com>
2025-06-11 02:22:24 -0500

    nix flake
    maybe eventually will add a `nix run` command

--- flake.nix
+++ flake.nix
@@ -1,28 +1,16 @@
 {
-  description = "Fully local & open source AI Waifu.";
+  description = "Development flake for z-waif.";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }@inputs:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ python311Full ];
-          shellHook = "./startup.sh";
-        };
-        packages.default = let startup = builtins.readFile ./startup.sh;
-        in pkgs.mkDerivation {
-          pname = "z-waif";
-          src = ./.;
-          nativeBuildInputs = with pkgs; [ python311 ];
-          my-script = (pkgs.writeScriptBin my-name my-src).overrideAttrs (old: {
-            buildCommand = ''
-              ${old.buildCommand}
-               patchShebangs $out'';
-          });
-
+          buildInputs = with pkgs; [ python311Full python311Packages.pip ];
+          # Load the venv in the .envrc file
         };
       });
 }
