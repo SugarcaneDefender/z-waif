52c4af2be34de100aa978ca4f2a9750ea32b1da5
Henry <cootshk@duck.com>
2025-07-24 11:11:56 -0500

    format + fix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Development flake for z-waif.";
   nixConfig = {
     extra-substituters = "https://z-waif.cachix.org";
-    extra-trusted-public-keys =
-      "z-waif.cachix.org-1:w4aiXXlhHNzyrV/tkrQvJHnBLLw8eWoYiPwJiNMcmI0=";
+    extra-trusted-public-keys = "z-waif.cachix.org-1:w4aiXXlhHNzyrV/tkrQvJHnBLLw8eWoYiPwJiNMcmI0=";
     download-buffer-size = 2147483648; # 2 GiB
   };
   inputs = {
@@ -12,8 +11,16 @@
     flake-utils.url = "github:numtide/flake-utils";
     pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
   };
-  outputs = { self, nixpkgs, flake-utils, pyproject-nix, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pyproject-nix,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         pypkgs = import nixpkgs {
@@ -22,96 +29,122 @@
             # e2fsprogs
             (final: prev: {
               # e2fsprogs = (import inputs.e2fspkgs {inherit system;}).e2fsprogs;
-              e2fsprogs =
-                ((prev.e2fsprogs.override { withFuse = true; }).overrideAttrs
-                  (final: prev: {
+              e2fsprogs = (
+                (prev.e2fsprogs.override { withFuse = true; }).overrideAttrs (
+                  final: prev: {
                     postInstall = (prev.postInstall or "") + ''
                       cp $fuse2fs/bin/fuse2fs $bin/bin
                     '';
                     doCheck = false; # tests fail on btrfs
-                  }));
+                  }
+                )
+              );
             })
             # Python
             (final: prev: rec {
               pythonPackagesOverlays = (prev.pythonPackagesOverlays or [ ]) ++ [
-                (python-final: python-prev:
+                (
+                  python-final: python-prev:
                   with python3.pkgs;
-                  pythonPackages // {
+                  pythonPackages
+                  // {
                     # Package renames
                     discord-py = discordpy;
                     silero-vad = pysilero-vad;
-                  })
+                  }
+                )
               ];
-              python3 = let
-                self = prev.python311.override {
-                  inherit self;
-                  packageOverrides =
-                    prev.lib.composeManyExtensions final.pythonPackagesOverlays;
-                };
-              in self;
-              python = python3;
-              python311 = python3;
-              python311Packages = python3.pkgs;
+              python311 =
+                let
+                  self = prev.python311.override {
+                    inherit self;
+                    packageOverrides = prev.lib.composeManyExtensions final.pythonPackagesOverlays;
+                  };
+                in
+                self;
+              python311Packages = python311.pkgs;
             })
           ];
         };
-        python3 = pypkgs.python3;
-        libs = with pkgs; [ portaudio libglvnd glib ];
+        python3 = pypkgs.python311;
+        libs = with pkgs; [
+          portaudio
+          libglvnd
+          glib
+        ];
         project = pyproject-nix.lib.project.loadRequirementsTxt {
           requirements = builtins.readFile ./requirements.txt;
           projectRoot = ./.;
         };
         pythonEnv =
           # assert project.validators.validateVersionConstraints { python = python3; } == { };
-          (python3.withPackages (project.renderers.withPackages {
-            python = python3;
-            pythonPackages = python3.pkgs;
-          }));
+          (
+            python3.withPackages (
+              project.renderers.withPackages {
+                python = python3;
+                pythonPackages = python3.pkgs;
+              }
+            )
+          );
         pythonPackages = with pkgs; {
           # mouse package for requirements.txt
-          mouse = let
-            version = "0.7.1";
-            pname = "mouse";
-          in python3.pkgs.buildPythonPackage {
-            inherit pname version;
-            src = fetchFromGitHub {
-              owner = "boppreh";
-              repo = "mouse";
-              tag = "v${version}";
-              hash = "sha256-vVUEmW7maSMONoVHym/6NPaxgOL87Br6/ue7nbd5XR8=";
-            };
-            propagatedBuildInputs = [ python3.pkgs.pynput ];
-          };
-          # Waiting on upstream to mark as linux supported
-          pyvts = let
-            version = "0.3.3";
-            pname = "pyvts";
-          in python3.pkgs.buildPythonPackage {
-            inherit pname version;
-            src = fetchPypi {
+          mouse =
+            let
+              version = "0.7.1";
+              pname = "mouse";
+            in
+            python3.pkgs.buildPythonPackage {
               inherit pname version;
-              sha256 = "sha256-3s8ymaORzi44/8w/ssr3Hwaye5yit1gdxal+YmINosQ=";
+              src = fetchFromGitHub {
+                owner = "boppreh";
+                repo = "mouse";
+                tag = "v${version}";
+                hash = "sha256-vVUEmW7maSMONoVHym/6NPaxgOL87Br6/ue7nbd5XR8=";
+              };
+              propagatedBuildInputs = [ python3.pkgs.pynput ];
             };
-            propagatedBuildInputs = [ ];
-          };
-          twitchio = let
-            version = "2.9.1";
-            pname = "twitchio";
-          in python3.pkgs.buildPythonPackage {
-            inherit pname version;
-            src = fetchFromGitHub {
-              owner = "PythonistaGuild";
-              repo = "twitchio";
-              tag = "v${version}";
-              hash = "sha256-JbRpIMwrmcpV4juEoI3epE+8n97XkKolFFpYsXmH7LE=";
+          # Waiting on upstream to mark as linux supported
+          pyvts =
+            let
+              version = "0.3.3";
+              pname = "pyvts";
+            in
+            python3.pkgs.buildPythonPackage {
+              inherit pname version;
+              src = fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-3s8ymaORzi44/8w/ssr3Hwaye5yit1gdxal+YmINosQ=";
+              };
+              propagatedBuildInputs = [ ];
             };
-            propagatedBuildInputs = [ ];
-          };
+          twitchio =
+            let
+              version = "2.9.1";
+              pname = "twitchio";
+            in
+            python3.pkgs.buildPythonPackage {
+              inherit pname version;
+              src = fetchFromGitHub {
+                owner = "PythonistaGuild";
+                repo = "twitchio";
+                tag = "v${version}";
+                hash = "sha256-JbRpIMwrmcpV4juEoI3epE+8n97XkKolFFpYsXmH7LE=";
+              };
+              propagatedBuildInputs = [ ];
+            };
         };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [ mypy python3 python311Packages.pip git ] ++ libs;
+          buildInputs =
+            with pkgs;
+            [
+              mypy
+              python3
+              python311Packages.pip
+              git
+            ]
+            ++ libs;
           packages = [ pythonEnv ];
           shellHook = ''
             export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.portaudio.out}
@@ -121,11 +154,19 @@
           # Load the venv in the .envrc file
         };
         # This really should be a seperate file lmao
-        packages = with pkgs;
+        packages =
+          with pkgs;
           rec {
-            inherit python3 python311Packages e2fsprogs pythonEnv;
+            inherit
+              python3
+              # python311Packages
+              e2fsprogs
+              pythonEnv
+              ;
             python = python3;
             python311 = python3;
-          } // pythonPackages;
-      });
+          }
+          // pythonPackages;
+      }
+    );
 }
